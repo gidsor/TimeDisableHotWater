f2019a1ccef4d98e62898927fb71427f743704a9
Vadim Denisov <gidsor@gmail.com>
2020-09-03 16:06:44 +0500

    Initial Commit

---
+++ TimeDisableHotWater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeDisableHotWater
+//
+//  Created by Vadim Denisov on 03.09.2020.
+//  Copyright © 2020 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
