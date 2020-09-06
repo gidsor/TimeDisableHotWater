dfdd08a08c73d7e9015c0f134ffe84f507e3cfdc
Vadim Denisov <gidsor@gmail.com>
2020-09-06 12:01:28 +0500

    Move all app strings to enum AppStrings

---
+++ TimeDisableHotWater/Constants/AppStrings.swift
@@ -0,0 +1,29 @@
+//
+//  AppStrings.swift
+//  TimeDisableHotWater
+//
+//  Created by Vadim Denisov on 06.09.2020.
+//  Copyright © 2020 Vadim Denisov. All rights reserved.
+//
+
+import Foundation
+
+enum AppStrings {
+    
+    /// Сроки отключения горячей воды
+    static let scheduleViewTitle = "Сроки отключения горячей воды"
+    /// Возникла проблема c интернетом. Попробуйте еще раз
+    static let networkErrorMessage = "Возникла проблема c интернетом. Попробуйте еще раз"
+    /// Возникла проблема с архивом. Попробуйте еще раз
+    static let networkArchiveMessage = "Возникла проблема с архивом. Попробуйте еще раз"
+    /// Ошибка
+    static let error = "Ошибка"
+    /// OK
+    static let ok = "OK"
+    /// дом
+    static let house = "дом"
+    /// корпус
+    static let housing = "корпус"
+    /// литер
+    static let liter = "литер"
+}
